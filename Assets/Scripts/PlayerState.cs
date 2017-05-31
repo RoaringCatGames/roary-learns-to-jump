328f1ea6cd450a91e77a05329472ee7e689017ae
Barryrowe <barryrowe@gmail.com>
2017-05-30 21:58:57 -0400

    Apply deceleration, Add respawn mechanism.
    Deceleration is being added manually. Trying to avoid Friction from the
    physics engine, but getting weird bugs.

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -8,6 +8,7 @@ public class PlayerState : MonoBehaviour {
 	public float jumpForce = 500f;
 	public float moveForce = 400f;
 	public float maxSpeed = 5f;
+	public float decelerationRate = 10f;
 	
 
 	private bool isGrounded = false;
@@ -15,7 +16,7 @@ public class PlayerState : MonoBehaviour {
 	private Rigidbody2D rigidBody;
 
 	// Use this for initialization
-	void Start () {
+	void Start () {		
 		rigidBody = GetComponent<Rigidbody2D>();
 		foreach(string name in Input.GetJoystickNames()){
 			Debug.Log("Joystick Button: " + name);
@@ -29,22 +30,32 @@ public class PlayerState : MonoBehaviour {
 			shouldJump = true;
 		}
 
-		for(int i = 0;i < 20; i++) {
-				if(Input.GetKeyDown("joystick 1 button " + i)){
-						print("joystick 1 button " + i);
-				}
-		}
+		// for(int i = 0;i < 20; i++) {
+		// 		if(Input.GetKeyDown("joystick 1 button " + i)){
+		// 				print("joystick 1 button " + i);
+		// 		}
+		// }
 	}
 
 	void FixedUpdate(){
 
 		float h = Input.GetAxis(Action.Move);
 
-		if (h * rigidBody.velocity.x < maxSpeed)
+		if (h != 0f && h * rigidBody.velocity.x < maxSpeed) {			  
 				rigidBody.AddForce(Vector2.right * h * moveForce);
+		}
 
-		if (Mathf.Abs (rigidBody.velocity.x) > maxSpeed)
-				rigidBody.velocity = new Vector2(Mathf.Sign (rigidBody.velocity.x) * maxSpeed, rigidBody.velocity.y);
+		if (Mathf.Abs (rigidBody.velocity.x) > maxSpeed){			
+				float newX = Mathf.Sign (rigidBody.velocity.x) * maxSpeed;				
+				rigidBody.velocity = new Vector2(newX, rigidBody.velocity.y);				
+		}
+
+		// if(h == 0f && rigidBody.velocity.x != 0f){
+		// 	float adjust = decelerationRate * Time.fixedDeltaTime * -(Mathf.Sign(rigidBody.velocity.x));			
+		// 	float adjustedX = adjust + rigidBody.velocity.x;			
+		// 	float targetX = Mathf.Sign(adjustedX) != Mathf.Sign(rigidBody.velocity.x) ? 0f : adjustedX;			
+		// 	rigidBody.velocity = new Vector2(targetX , rigidBody.velocity.y);			
+		// }
 
 		if(shouldJump) {
 			rigidBody.AddForce(new Vector2(0f, jumpForce));
---
+++ Assets/Scripts/Respawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour {
+
+	public float respawnTriggerY = -10f;
+	public Vector2 respawnPosition = new Vector2(1f, 1f);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.y <= respawnTriggerY){
+			transform.position = respawnPosition;
+		}
+	}
+}
